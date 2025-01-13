c8c71e6aa0b00fe4fb9e843050ed58053612b473
ribei <ribei@RUCA>
2025-01-13 23:57:28 +0000

    Add project files.

---
+++ BasicCalculator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BasicCalculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
