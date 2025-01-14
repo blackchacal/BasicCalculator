524490d6c6cfaccf1162acb3e68bcc3ad93480bb
blackchacal <ribeiro.tonet@gmail.com>
2025-01-14 00:47:07 +0000

    Add UI.

--- BasicCalculator/MainWindow.xaml.cs
+++ BasicCalculator/MainWindow.xaml.cs
@@ -20,5 +20,90 @@ namespace BasicCalculator
         {
             InitializeComponent();
         }
+
+        private void btn_plus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_minus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_prod_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_div_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_res_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_pm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_dot_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
